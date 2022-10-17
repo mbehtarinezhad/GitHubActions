8fd7765cb2ac96282af841bc769b678c267562d7
Mohammad Behtarinezhad <m.behtarinezhad@bettechnology.com>
2022-10-17 11:25:30 +0330

    add sample class

--- GitHubActions/WeatherForecast.cs
+++ GitHubActions/WeatherForecast.cs
@@ -9,5 +9,18 @@ namespace GitHubActions
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        void Test()
+        {
+            var a = new Test("asd");
+        }
+    }
+
+    public class Test
+    {
+        public Test(string a)
+        {
+
+        }
     }
 }
